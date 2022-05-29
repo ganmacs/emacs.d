a70fe9a950802de6773fa36c54c583e3d7f21a63
Yuta Iwama <ganmacs@gmail.com>
2022-05-29 23:47:11 +0900

    remove unused snippets

--- inits/20_git.el
+++ inits/20_git.el
@@ -10,4 +10,18 @@
   :config
   (global-git-gutter-mode))
 
+(defun open-github-commit ()
+  (interactive)
+  (let* ((cmd1 "git blame -l -L %s,+1  %s | cut -d ' ' -f 1")
+         (cmd2 "hub browse -- commit/%s")
+         (zero "0000000000000000000000000000000000000000")
+         (commit-id (shell-command-to-string
+                     (format cmd1 (line-number-at-pos) buffer-file-name))))
+    (if (string= zero commit-id)
+        (message "This line is not commited")
+      (shell-command (format cmd2 commit-id)))))
+
+(global-set-key (kbd "M-g o") 'open-github-commit)
+
+
 ;;; 20_git.el ends here
